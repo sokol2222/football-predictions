0b2d755b385bfd2e72de81792dd90f8217b75117
Sokol <dvsokol22@gmail.com>
2026-05-28 18:12:25 +0300

    github pages

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,5 @@ import { cloudflare } from "@cloudflare/vite-plugin";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), cloudflare()],
+  base: '/football-predictions/'
 })
