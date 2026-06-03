3de6e0e2de649522f264923970eb073404fef3ba
Dmitriy Sokolov <dvsokol22@gmail.com>
2026-06-04 00:20:31 +0300

    proxy

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-//const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseUrl = 'https://cors-anywhere.herokuapp.com/https://wxawzuyxnydzbuklaald.supabase.co';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
